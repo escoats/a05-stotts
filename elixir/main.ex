407eed1a079f5cd1db365bfc9e447c33f5d921cb
Elizabeth Coats <escoats@unc.edu>
2026-03-11 22:21:56 -0400

    Implement rings and nodes

--- elixir/main.ex
+++ elixir/main.ex
@@ -1,63 +1,106 @@
-# Implementation here!
 defmodule FourRings do
     def start(n, h) do
         IO.puts("N = #{n}")
         IO.puts("H = #{h}")
 
-        # spawn & wire together nodes
+        # spawn rings with transform functions
+        # keep hold of pid to "gatekeeper" functions
+        neg_gk  = build_ring(n, h, fn v -> v * 3 + 1 end)
+        zero_gk = build_ring(n, h, fn v -> v + 7 end)
+        even_gk = build_ring(n, h, fn v -> v * 101 end)
+        odd_gk  = build_ring(n, h, fn v -> v * 101 + 1 end)
 
+        # prompt for input (recursive loop)
+        input_loop(neg_gk, zero_gk, even_gk, odd_gk, 1)
+    end
 
-        # prompt for input
-        input = IO.gets("Enter a number: \n")
-        {num, _} = Integer.parse(input)
-        cond do
-            input == "done" ->
-                # TODO: stop accepting new input
-                IO.puts("Shutting down")
-            num < 0 ->
-                IO.puts("Sending to negative ring")
-            num == 0 ->
-                IO.puts("Sending to zero ring")
-            num > 0 && (Integer.mod(num, 2) == 0) ->
-                IO.puts("send to even ring")
-            num > 0 && (Integer.mod(num, 2) != 0) ->
-                IO.puts("Send to odd ring")
+    def input_loop(neg_gk, zero_gk, even_gk, odd_gk, token_counter) do
+        input = IO.gets("Enter a number: \n") |> String.trim()
+
+        if input == "done" do
+            # :done goes into each gatekeeper's mailbox after any queued :input messages,
+            # so all in-flight and queued work finishes before shutdown
+            send(neg_gk, :done)
+            send(zero_gk, :done)
+            send(even_gk, :done)
+            send(odd_gk, :done)
+            IO.puts("Shutting down")
+        else
+            case Integer.parse(input) do
+                {num, _} ->
+                    cond do
+                        num < 0 ->
+                            send(neg_gk, {:input, token_counter, :neg, num})
+                        num == 0 ->
+                            send(zero_gk, {:input, token_counter, :zero, num})
+                        num > 0 && rem(num, 2) == 0 ->
+                            send(even_gk, {:input, token_counter, :even, num})
+                        num > 0 && rem(num, 2) != 0 ->
+                            send(odd_gk, {:input, token_counter, :odd, num})
+                    end
+                    input_loop(neg_gk, zero_gk, even_gk, odd_gk, token_counter + 1)
+
+                :error ->
+                    IO.puts("Error: '#{input}' is not a valid integer")
+                    input_loop(neg_gk, zero_gk, even_gk, odd_gk, token_counter)
+            end
         end
     end
 
-    def neg_gatekeeper(node_1_PID) do
+    def build_ring(n, h, transform) do
+        pids = Enum.map(1..n, fn _ -> spawn(FourRings, :node_init, [transform]) end)
+
+        pids
+        |> Enum.with_index()
+        |> Enum.each(fn {pid, i} ->
+            next = Enum.at(pids, rem(i + 1, n))
+            send(pid, {:set_next, next})
+        end)
+
+        first = hd(pids)
+        spawn(FourRings, :gatekeeper, [first, h])
+    end
+
+    # --- Gatekeeper ---
+
+    # enforces one-token-at-a-time rule using nested receive blocks
+    def gatekeeper(node_1, h) do
         receive do
-            {token_id, ring_id, orig_input, h} ->
-            current_val = orig_input
-            remaining_hops = h
-            send(node_1_PID, {token_id, ring_id, orig_input, current_val, remaining_hops})
+            {:input, token_id, ring_id, value} ->
+                send(node_1, {:token, token_id, ring_id, value, value, h, self()})
 
-            # wait for completion message from same token
-            receive do
-              {:complete, ^token_id, orig_input, final_val} ->
-                IO.puts("Token #{token_id} completed in ring #{ring_id}: #{orig_input} -> #{final_val}")
-            end
-            neg_gatekeeper(node_1_PID)
+                # process is blocked until "complete" message comes through
+                receive do
+                    {:complete, ^token_id, ring_id, orig_input, final_val} ->
+                        IO.puts("Token #{token_id} ring #{ring_id}: #{orig_input} -> #{final_val}")
+                end
+                gatekeeper(node_1, h)
 
-            "done" ->
-                send(node_1_PID, "done")
+            :done ->
                 :ok
         end
     end
 
-    def neg_node(next_PID) do
+    # --- Nodes ---
+
+    def node_init(transform) do
         receive do
-            {token_id, ring_id, orig_input, current_val, remaining_hops} ->
-                new_val = current_val*3 + 1
+            {:set_next, next_pid} -> node_loop(next_pid, transform)
+        end
+    end
+
+    def node_loop(next_pid, transform) do
+        receive do
+            {:token, token_id, ring_id, orig_input, current_val, remaining_hops, gk} ->
+                new_val = transform.(current_val)
                 hops_left = remaining_hops - 1
 
                 if hops_left > 0 do
-                    # forward to next node
-                    send(next_PID, {token_id, ring_id, orig_input, new_val, hops_left})
+                    send(next_pid, {:token, token_id, ring_id, orig_input, new_val, hops_left, gk})
                 else
-                    IO.puts("Report completion to manager process")
+                    send(gk, {:complete, token_id, ring_id, orig_input, new_val})
                 end
         end
-        neg_node(next_PID)
+        node_loop(next_pid, transform)
     end
 end
